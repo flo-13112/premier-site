a71124817c75b5ba1342aa0b15e7ddcd0e063552
razer <florent.ezelis@digital-network.net>
2019-08-16 14:34:24 +0200

    modifié :         mysite/settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!mfd^ktc&52u9_%a-pj$av@yzwug$t2e)@=(e*7i4vnsqpuqy9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost','192.168.2.228','razer.pythonanywhere.com','127.0.0.1']
 
